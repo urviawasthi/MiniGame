831c86e939956fe6e95886a94b0de585ca05de4a
urvi <urvia2@illinois.edu>
2019-11-30 22:40:53 -0600

    created Game Activity and linked play button to game activity

--- app/src/main/java/com/example/minigame/MainActivity.java
+++ app/src/main/java/com/example/minigame/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.minigame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** This is the play button on the main screen of the minigame. */
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //making the app landscape mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //when play button is clicked, launch a new activity: game activity
+        // 1) get the button
+        playButton = findViewById(R.id.playButton);
+        // 2) attach an onclick listener to the button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //when the play button is clicked, launch game activity
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
