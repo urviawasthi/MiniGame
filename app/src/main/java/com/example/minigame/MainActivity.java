04b44ee8f6455724cd7ecb857eeedd87974a5b17
urvi <urvia2@illinois.edu>
2019-12-01 15:22:46 -0600

    Made a Scores activity for the high scores, and linked that activity to the main activity.

--- app/src/main/java/com/example/minigame/MainActivity.java
+++ app/src/main/java/com/example/minigame/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     /** This is the play button on the main screen of the minigame. */
     private Button playButton;
 
+    /** This is the high scores button on the main screen of the minigame. */
+    private Button highScoresButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         //when play button is clicked, launch a new activity: game activity
-        // 1) get the button
         playButton = findViewById(R.id.playButton);
-        // 2) attach an onclick listener to the button
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 //when the play button is clicked, launch game activity
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //when high scores button is clicked, launch a new activity: scores activity
+        highScoresButton = findViewById(R.id.highScoresButton);
+        highScoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //when the high scores button is clicked, launch scores activity
+                Intent intent = new Intent(MainActivity.this, Scores.class);
                 startActivity(intent);
             }
         });
