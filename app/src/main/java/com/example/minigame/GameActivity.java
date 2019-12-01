9a78382fb54ec23f7e889549a96fbc73b8d9f1c9
urvi <urvia2@illinois.edu>
2019-12-01 15:05:34 -0600

    Added GameView to GameActivity

--- app/src/main/java/com/example/minigame/GameActivity.java
+++ app/src/main/java/com/example/minigame/GameActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
+    /** Declaring the game view. */
+    private GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 }
