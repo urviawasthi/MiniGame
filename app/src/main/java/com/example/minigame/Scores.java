d6aacd75532fd10f56182e660b6682a09e4e6f5b
ZornitsaA <za7@illinois.edu>
2019-12-02 22:42:19 -0600

    zoey is trying to commit

--- app/src/main/java/com/example/minigame/Scores.java
+++ app/src/main/java/com/example/minigame/Scores.java
@@ -2,7 +2,9 @@ package com.example.minigame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Scores extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Scores extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
