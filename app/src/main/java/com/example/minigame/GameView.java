c6942d1f2fc6c6799e831fe704b1737178decde3
urvi <urvia2@illinois.edu>
2019-12-02 22:36:17 -0600

    finished "pause" dialog

--- app/src/main/java/com/example/minigame/GameView.java
+++ app/src/main/java/com/example/minigame/GameView.java
@@ -108,6 +108,26 @@ public class GameView extends SurfaceView implements Runnable {
             gameThread.join();
         } catch (InterruptedException e) {
         }
+
+        //also show a dialog that tells the user the game is paused, and asks them to resume
+        AlertDialog.Builder builder = new AlertDialog.Builder(gameContext);
+        builder.setMessage("Game Paused!");
+        // Add the buttons
+        builder.setPositiveButton("Resume", new DialogInterface.OnClickListener() {
+            //user clicked resume
+            public void onClick(DialogInterface dialog, int id) {
+                resume();
+            }
+        });
+        builder.setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+            // user clicked quit
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(gameContext, MainActivity.class);
+                gameContext.startActivity(intent);
+            }
+        });
+
+        builder.create().show();
     }
 
     /**
