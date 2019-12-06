9393a0424b390a17c40c6456f5223b893ecd0df5
urvi <urvia2@illinois.edu>
2019-12-05 18:36:24 -0600

    added collision stuff + getters and setters for character sprite and geoff

--- app/src/main/java/com/example/minigame/CharacterSprite.java
+++ app/src/main/java/com/example/minigame/CharacterSprite.java
@@ -4,6 +4,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -23,46 +24,14 @@ public class CharacterSprite {
     private double yDisplacement;
     private double slope;
     private int speed;
+    private Rect detectCollision;
 
     public CharacterSprite(Bitmap bmp) {
         image = bmp;
         image = Bitmap.createScaledBitmap(
                 image, characterWidth, characterHeight, false);
         generatePosition();
-        System.out.println("quadrant is " + quadrant + " and edge is " + edge);
-        if (edge == 1 && quadrant == 1) {
-            xPosition = generateXPosition();
-            yPosition = 0;
-        }
-        if (edge == 1 && quadrant == 2) {
-            xPosition = generateXPosition();
-            yPosition = 0;
-        }
-        if (edge == 1 && quadrant == 3) {
-            xPosition = generateXPosition();
-            yPosition = screenHeight;
-        }
-        if (edge == 1 && quadrant == 4) {
-            xPosition = generateXPosition();
-            yPosition = screenHeight;
-        }
-        if (edge == 2 && quadrant == 1) {
-            yPosition = generateYPosition();
-            xPosition = screenWidth;
-        }
-        if (edge == 2 && quadrant == 2) {
-            yPosition = generateYPosition();
-            xPosition = 0;
-        }
-        if (edge == 2 && quadrant == 3) {
-            yPosition = generateYPosition();
-            xPosition = 0;
-        }
-        if (edge == 2 && quadrant == 4) {
-            yPosition = generateYPosition();
-            xPosition = screenWidth;
-        }
-        speed = 1;
+        speed = 5;
         //initial x and y velocites, without scaling
         xDisplacement = Math.abs(xPosition - (screenWidth / 2));
         yDisplacement = Math.abs((screenHeight / 2) - yPosition);
@@ -70,7 +39,12 @@ public class CharacterSprite {
         //scaling the velocities
         slope = yDisplacement / xDisplacement * speed;
 
+        // detecting collision
         isCrashing = false;
+
+        //the collision object will be a rectangle
+        detectCollision = new Rect((int) xPosition, (int) yPosition, image.getWidth(), image.getHeight());
+
     }
 
     public void draw(Canvas canvas) {
@@ -99,6 +73,13 @@ public class CharacterSprite {
                 xPosition = xPosition - speed;
                 yPosition = yPosition - slope;
             }
+
+            //detecting collision between enemy and geoff
+            detectCollision.left = (int) xPosition;
+            detectCollision.top = (int) yPosition;
+            detectCollision.right = (int) xPosition + image.getWidth();
+            detectCollision.bottom = (int) yPosition + image.getHeight();
+
             //depending on which wave it is, change the SPEED
             //depending on where it's coming from, DIFF X AND Y
             //redraw
@@ -137,9 +118,55 @@ public class CharacterSprite {
         }
     }
 
+    //generates the quadrant and which edge the enemy will approach from
+    //based on those values, generates a position accordingly
     private void generatePosition() {
         Random rand = new Random();
         quadrant = rand.nextInt(4) + 1;
         edge = rand.nextInt(2) + 1;
+
+        if (edge == 1 && quadrant == 1) {
+            xPosition = generateXPosition();
+            yPosition = 0;
+        }
+        if (edge == 1 && quadrant == 2) {
+            xPosition = generateXPosition();
+            yPosition = 0;
+        }
+        if (edge == 1 && quadrant == 3) {
+            xPosition = generateXPosition();
+            yPosition = screenHeight;
+        }
+        if (edge == 1 && quadrant == 4) {
+            xPosition = generateXPosition();
+            yPosition = screenHeight;
+        }
+        if (edge == 2 && quadrant == 1) {
+            yPosition = generateYPosition();
+            xPosition = screenWidth;
+        }
+        if (edge == 2 && quadrant == 2) {
+            yPosition = generateYPosition();
+            xPosition = 0;
+        }
+        if (edge == 2 && quadrant == 3) {
+            yPosition = generateYPosition();
+            xPosition = 0;
+        }
+        if (edge == 2 && quadrant == 4) {
+            yPosition = generateYPosition();
+            xPosition = screenWidth;
+        }
+    }
+
+    //setting the xPosition so that we can move the bitmap away as soon as collision is detected
+    //or so that we can move it away when user taps on it
+    private void setXPosition(double setXPosition) {
+        xPosition = setXPosition;
+    }
+
+    //returns the detect collision rectangle so that we can detect collision in different quadrants
+    private Rect getDetectCollision() {
+        return detectCollision;
     }
 }
